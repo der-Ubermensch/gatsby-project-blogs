ba7797e363ab320efdbccbef2855d52d35a05684
der-Ubermensch <philipfkimani@gmail.com>
2021-02-27 14:49:32 +0100

    Add gatsby-source-filesystem Plugin (Amended).

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,24 @@ module.exports = {
           {
             resolve: `gatsby-source-filesystem`,
             options: {
-              name: `pages`,
-              path: `${__dirname}/src/pages/`,
+              name: `src`,
+              path: `${__dirname}/src/`,
+            },
+          },
+
+          {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              // CommonMark mode (default: true)
+              commonmark: true,
+              // Footnotes mode (default: true)
+              footnotes: true,
+              // Pedantic mode (default: true)
+              pedantic: true,
+              // GitHub Flavored Markdown mode (default: true)
+              gfm: true,
+              // Plugins configs
+              plugins: [],
             },
           },
 
