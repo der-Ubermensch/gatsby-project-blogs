97b3ed95f84269a4deb25cf5fa1d2a758ff6f0af
der-Ubermensch <philipfkimani@gmail.com>
2021-02-28 21:02:48 +0100

    Render contentful posts.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -12,28 +12,44 @@ const  BlogPage = () => {
         allMarkdownRemark {
           edges {
             node {
-              frontmatter {
-                title
-                date
-              }
-              fields {
-                slug
-              }
+              frontmatter { title date }
+              fields { slug }
             }
+          }
+        }
+
+        allContentfulBlogPost ( sort: { fields: publishedDate, order: DESC } ){
+          edges {
+            node { title slug publishedDate(formatString:"MMMM Do, YYYY") }
           }
         }
       }`);
 
-    const blogs = data.allMarkdownRemark.edges.map((edge) => {
+    const blogsMd = data.allMarkdownRemark.edges.map((edge) => {
+      const {slug, title, date } = { ...edge.node.fields, ...edge.node.frontmatter }
         return (
         <li className={blogStyles.post}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+            <Link to={`/blog/${slug}`}>
+              <h2>{title}</h2>
+              <h6>MD</h6>
+              <p>{date}</p>
             </Link>
         </li>
         )
     });
+
+    const blogsCms = data.allContentfulBlogPost.edges.map((edge) => {
+      const { slug, title, publishedDate } = edge.node
+      return (
+      <li className={blogStyles.post}>
+          <Link to={`/blog/${slug}`}>
+            <h2>{title}</h2>
+            <h6>CMS</h6>
+            <p>{publishedDate}</p>
+          </Link>
+      </li>
+      )
+  });
 
 
     return (
@@ -42,7 +58,8 @@ const  BlogPage = () => {
         <h1>Blog</h1>
         <p>Posts will show up here later on.</p>
         <ol className={blogStyles.posts}>
-          { blogs }
+          { blogsMd }
+          { blogsCms }
         </ol>
 
         </Layout>
